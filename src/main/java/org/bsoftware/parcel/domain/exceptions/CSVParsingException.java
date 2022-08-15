7b353495355422e2342ce428c3f9e194b11c5a8a
Rudolf Barbu <rudolf_barbu@outlook.com>
2022-08-15 17:00:03 +0300

    Refactorings

--- src/main/java/org/bsoftware/parcel/domain/exceptions/CSVParsingException.java
+++ src/main/java/org/bsoftware/parcel/domain/exceptions/CSVParsingException.java
@@ -4,14 +4,14 @@ package org.bsoftware.parcel.domain.exceptions;
  * CSVParsingException indicates that application can't parse the CSV file
  *
  * @author Rudolf Barbu
- * @version 1.0.0
+ * @version 1.0.1
  */
 public class CSVParsingException extends RuntimeException
 {
     /**
      * Defines message pattern for exception
      */
-    private static final String PATTERN = "Can't parse CSV file, clause: %s";
+    private static final String MESSAGE_PATTERN = "Can't parse CSV file, clause: %s";
 
     /**
      * Call super class with customized exception message
@@ -20,6 +20,6 @@ public class CSVParsingException extends RuntimeException
      */
     public CSVParsingException(final String details)
     {
-        super(String.format(PATTERN, details));
+        super(String.format(MESSAGE_PATTERN, details));
     }
 }
--- src/main/java/org/bsoftware/parcel/domain/exceptions/OperatingSystemNotSupportedException.java
+++ src/main/java/org/bsoftware/parcel/domain/exceptions/OperatingSystemNotSupportedException.java
@@ -4,14 +4,14 @@ package org.bsoftware.parcel.domain.exceptions;
  * OperatingSystemNotSupportedException indicates that current operating system is unsupported
  *
  * @author Rudolf Barbu
- * @version 1.0.1
+ * @version 1.0.2
  */
 public final class OperatingSystemNotSupportedException extends RuntimeException
 {
     /**
      * Defines message pattern for exception
      */
-    private static final String PATTERN = "Current operating system: %s is not supported";
+    private static final String MESSAGE_PATTERN = "Current operating system: %s is not supported";
 
     /**
      * Call super class with customized exception message
@@ -20,6 +20,6 @@ public final class OperatingSystemNotSupportedException extends RuntimeException
      */
     public OperatingSystemNotSupportedException(final String operatingSystem)
     {
-        super(String.format(PATTERN, operatingSystem));
+        super(String.format(MESSAGE_PATTERN, operatingSystem));
     }
 }
