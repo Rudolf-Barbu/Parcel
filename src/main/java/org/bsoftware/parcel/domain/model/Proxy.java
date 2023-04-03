63141596b757bda40d8a5cc5bfa094a46a35b9f2
Rudolf Barbu <Rudolf_Barbu@outlook.com>
2023-04-03 11:40:30 +0300

    Sensitive data removed.

--- src/main/java/org/bsoftware/parcel/domain/model/Proxy.java
+++ src/main/java/org/bsoftware/parcel/domain/model/Proxy.java
@@ -1,6 +1,5 @@
 package org.bsoftware.parcel.domain.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -8,10 +7,9 @@ import lombok.RequiredArgsConstructor;
  * Source is a class that is used for representing source item
  *
  * @author Rudolf Barbu
- * @version 3
+ * @version 4
  */
 @Getter
-@EqualsAndHashCode
 @RequiredArgsConstructor
 public final class Proxy
 {
